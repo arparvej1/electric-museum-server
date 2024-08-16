081f27a648907ea0b63f3317dcfd11a741c12447
arparvej1 <parvej8pro@gmail.com>
2024-08-16 18:58:39 +0600

    Update product image api add

--- index.js
+++ index.js
@@ -122,6 +122,21 @@ async function run() {
       res.send(result);
     });
 
+    // Update product - put / update picture
+    app.put('/newPicture/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true };
+      const updatedProduct = req.body;
+
+      const product = {
+        $set: {
+          ProductImage: updatedProduct.newPicture
+        }
+      }
+      const result = await productCollection.updateOne(filter, product, options);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
