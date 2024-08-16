47c06f37f565f6a2e70fcacdd1580e2af7c294ec
arparvej1 <parvej8pro@gmail.com>
2024-08-16 10:53:41 +0600

    mongoDB setup

--- index.js
+++ index.js
@@ -1,7 +1,54 @@
 const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://electric-museum.web.app',
+    'https://electric-museum.firebaseapp.com'
+  ],
+  credentials: true
+}));
+app.use(express.json());
+
+const uri = `${process.env.MONGODB_EM_CONNECTION}`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+
+    const mainDB = client.db('ElectricMuseumDB'); // << ----- main Database here -----
+    const userCollection = mainDB.collection('users');
+
+    // All Users ------------------
+    app.get('/users', async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir);
+
+// ---------- Server is running .......
 app.get('/',  (req, res) => {
   res.send('Server is running...')
 });
